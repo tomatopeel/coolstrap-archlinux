db2baa0e6f7a9e108d1b11828de5815186d2a205
tomatopeel <tom@tomatopeel.com>
2017-11-23 13:05:41 +0100

    Amending packages.both with packages.

--- cool-build.sh
+++ cool-build.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
+# add_packages FILE PKGS...
+add_packages() {
+  file="$1"
+  shift 1
+  printf "%s\n" "$@" >> "$file"
+}
+
 cwd=$(dirname $(readlink -f $0))
+mkdir out; cd out
+
+git clone https://git.archlinux.org/archiso.git
+pkgsboth='archiso/configs/releng/packages.both'
 
 selection=$(whiptail --title "Software Selection" --checklist \
   "Please select the desired wallet software to install" 20 78 3 \
-  "bitcoin-cli bitcoin-daemon" "Bitcoin Core" ON \
-  "monero" "Monero" ON \
-  "electrum" "Electrum" ON \
+  "btc_core" "Bitcoin Core" ON \
+  "btc_electrum" "Electrum" ON \
+  "xmr" "Monero" ON \
   3>&1 1>&2 2>&3 | sed 's/"//g' | sed 's/ /\n/g')
 
 while read -r line; do
   case $line in
-    monero)
+    btc_core)
+      add_packages "$pkgsboth" bitcoin-cli bitcoin-daemon
+      ;;
+    btc_electrum)
+      add_packages "$pkgsboth" electrum
+      ;;
+    xmr)
       git clone https://github.com/monero-project/monero
       cd monero
       make
       ;;
-    bitcoin-cli)
-      echo "bitcoin core!"
-      ls
-      ;;
-    electrum)
-      echo "electrum!"
-      ls
-      ;;
   esac
 done <<< "$selection"
+
+#cd $cwd; rm -rf out
 
 #echo "$selection" >> archiso/configs/releng/packages.both
 #if [[ $selection == *"monero"* ]]; then
