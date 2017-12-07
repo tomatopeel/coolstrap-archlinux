f39a5bd1b1bbdcd178033b5b9bfc7e9686b336f6
tomatopeel <tom@tomatopeel.com>
2017-12-07 13:54:21 +0100

    Bash script to generate Bitcoin Core wallet/addresses.

---
+++ scripts/bitcoin-core.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+error_exit() {
+  echo "$0: $1" 1>&2
+  exit 1
+}
+
+repeat-try() {
+  n=0
+  until [ $n -ge 5 ]
+  do
+    "$@" && return
+    n=$[$n+1]
+    printf "%s\nTrying again, attempt #$n...\n" "$msg"
+    sleep "$n"
+  done
+  error_exit "$msg"
+}
+
+pass generate bitcoin-script-test -f 32 1>/dev/null 2>&1 ||
+  error_exit "$LINENO: Couldn't gen pw"
+PW=$(pass bitcoin-script-test || error_exit "$LINENO: couldn't get pw")
+
+DATADIR="$(pwd)/data"
+mkdir "$DATADIR" || error_exit "$LINENO: couldn't mkdir data"
+
+msg="$((LINENO+1)): couldn't start bitcoind"
+repeat-try bitcoind -daemon -datadir="$DATADIR"
+
+msg="$((LINENO+1)): couldn't encryptwallet"
+repeat-try bitcoin-cli -datadir="$DATADIR" encryptwallet "$PW"
+
+msg="$((LINENO+1)): couldn't start bitcoind"
+repeat-try bitcoind -daemon -datadir="$DATADIR"
+
+msg="$((LINENO+1)): couldn't walletpassphrase"
+repeat-try bitcoin-cli -datadir="$DATADIR" walletpassphrase "$PW" 300
+
+msg="$((LINENO+1)): couldn't backupwallet"
+repeat-try bitcoin-cli -datadir="$DATADIR" backupwallet backup.dat
+
+msg="$((LINENO+1)): couldn't stop"
+repeat-try bitcoin-cli -datadir="$DATADIR" stop
+
+msg="$((LINENO+1)): couldn't cp"
+cp -f backup.dat "$DATADIR/wallet.dat" || error_exit "$msg"
+
+msg="$((LINENO+1)): couldn't start bitcoind"
+repeat-try bitcoind -daemon -datadir="$DATADIR"
+
+msg="$((LINENO+1)): couldn't walletpassphrase"
+repeat-try bitcoin-cli -datadir="$DATADIR" walletpassphrase "$PW" 300
+
+msg="$((LINENO+1)): couldn't stop"
+repeat-try bitcoin-cli -datadir="$DATADIR" listreceivedbyaddress 0 true |
+  jq .[].address -r > addrs
